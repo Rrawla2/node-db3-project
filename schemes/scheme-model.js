071820c07f7111418491a1c8cec318a975967aae
Kristi Gribble <kgribble4083@gmail.com>
2020-04-14 21:08:58 -0500

    Adding model exports

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -8,3 +8,21 @@ module.exports = {
     update,
     remove
 }
+
+function find() {
+    return db("schemes")
+}
+
+function findById(id) {
+    return db("schemes")
+    .where({ id })
+}
+
+function findSteps(id) {
+    return db("schemes")
+    .where({ id })
+}
+
+function add(scheme) {
+    return db("schemes").insert(scheme)
+}
